a86d052e8dcec1019760804026afecb46cd0584d
MacBook <macbook@Tiskaes-MacBook-Pro.local>
2023-02-08 15:12:02 +0100

    resumed learning

---
+++ 03 - Sorting algos/insertionSort.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array<number>} arr "array to be sorted"
+ * @returns {Array<number>} "the sorted array"
+ */
+function insertionSort(arr) {
+  // Pick the 2nd el in the arr
+  // Compare with the first el, and swap if necessary
+  // Continue to the next element and if it is in the incorrect order, iterate through the sorted portion...
+  //   ... to place the elemnet in the correct order
+}
+
+/**
+ * @type {Array<number>}
+ */
+const randomArr = []
